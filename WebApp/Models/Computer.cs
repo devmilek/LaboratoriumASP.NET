597dc1656e39004982ee7af37eac88d52707ec64
devmilek <devmilek@gmail.com>
2024-10-09 15:49:44 +0200

    feat: create computer class

---
+++ WebApp/Models/Computer.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Models;
+
+public class Computer
+{
+    [HiddenInput]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać imię!")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Proszę uzupełnić procesor!")]
+    public string Processor { get; set; }
+
+    [Required(ErrorMessage = "Proszę uzupełnić pamięć!")]
+    public string Memory { get; set; }
+
+    [Required(ErrorMessage = "Proszę uzupełnić kartę graficzną!")]
+    public string GraphicsCard { get; set; }
+
+    [Required(ErrorMessage = "Proszę uzupełnić producenta!")]
+    public string Manufacturer { get; set; }
+
+    [Required(ErrorMessage = "Proszę uzupełnić datę produkcji!")]
+    [DataType(DataType.Date)]
+    public DateTime ProductionDate { get; set; }
+}
