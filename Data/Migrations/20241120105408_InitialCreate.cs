0d76fcecd6ec0f5ef851d28276394689eb70a06b
devmilek <devmilek@gmail.com>
2025-01-09 13:31:05 +0100

    new updates

---
+++ Data/Migrations/20241120105408_InitialCreate.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "organizations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Regon = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Nip = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Address_City = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Address_Street = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Address_PostalCode = table.Column<string>(type: "TEXT", maxLength: 10, nullable: false),
+                    Address_Region = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_organizations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "computers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Processor = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Memory = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    GraphicsCard = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Manufacturer = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    production_date = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValue: new DateTime(2024, 11, 20, 11, 54, 7, 940, DateTimeKind.Local).AddTicks(1760)),
+                    OrganizationId = table.Column<int>(type: "INTEGER", nullable: false, defaultValue: 101)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_computers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_computers_organizations_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "organizations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "organizations",
+                columns: new[] { "Id", "Address_City", "Address_PostalCode", "Address_Region", "Address_Street", "Nip", "Regon", "Title" },
+                values: new object[,]
+                {
+                    { 101, "Kraków", "31-150", "małopolskie", "Św. Filipa 17", "83492384", "13424234", "WSEI" },
+                    { 102, "Kraków", "31-150", "małopolskie", "Krowoderska 45/6", "2498534", "0873439249", "Firma" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "computers",
+                columns: new[] { "Id", "GraphicsCard", "Manufacturer", "Memory", "Name", "OrganizationId", "Processor", "production_date" },
+                values: new object[,]
+                {
+                    { 1, "Nvidia", "Dell", "8GB", "Kompuer1", 101, "Intel", new DateTime(2020, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, "AMD Radeon", "HP", "16GB", "Kompuer2", 102, "AMD", new DateTime(2021, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_computers_OrganizationId",
+                table: "computers",
+                column: "OrganizationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "computers");
+
+            migrationBuilder.DropTable(
+                name: "organizations");
+        }
+    }
+}
